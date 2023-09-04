d7e478ea5d6649e6ebde695ae9977431116bcdbc
Mr. Web Developer <117938690+Yahyobek-Coder@users.noreply.github.com>
2023-09-04 19:35:11 +0500

    Update blog.php

--- blog.php
+++ blog.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' &&  isset($_POST['DELTE'])) {
             <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
             <p>
                 <a href="post-create.php" class="btn btn-primary my-2">Post Yaratish</a>
-                <a href="#" class="btn btn-secondary my-2">Bosh Sahifa</a>
+                <a href="index.php" class="btn btn-secondary my-2">Bosh Sahifa</a>
             </p>
         </div>
     </div>
